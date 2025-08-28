f9a36adf346f5060596fa6827f7ac885cb8dd5e8
Alex <alex.cambrini@studio.unibo.it>
2025-08-28 20:00:19 +0200

    refactor: move main training script to utils and update logging/data flow - moved main.py functionality into utils - replaced temporary logger with flush_temp_logger - integrated SummaryWriter and LoggerUtils - updated DataLoaderManager, Trainer, Tester usage - replaced get_run_name with decide_run_name

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-import time
+import os
 import numexpr
 from pathlib import Path
 import sys
 import logging
-
+from utils.logging_utils import LoggerUtils
+from tester import Tester
 from utils.config_loader import ConfigLoader
-from data.data_loader import DataLoaderManager
-from train import Trainer
-from logger import get_logger
+from utils.data_loader import DataLoaderManager
+from trainer import Trainer
+from utils.logger import flush_temp_logger, get_logger
 from utils.dataset_analysis import DatasetAnalyzer
-from run_info import get_run_name
+from torch.utils.tensorboard import SummaryWriter
+from utils.model_utils import create_model
+from utils.run_info import decide_run_name
 
 def main():
     numexpr.set_num_threads(16)
@@ -30,38 +33,75 @@ def main():
         temp_logger.error("Configurazione non valida, esco.")
         sys.exit(1)
 
-    # --- Caricamento dati ---
-    data_manager = DataLoaderManager(config, temp_logger)
-    data_manager.load_data()
-    num_classes = len(data_manager.classes)
-    if data_manager.train_loader is None:
-        temp_logger.error("Errore: caricamento dati fallito. Esco.")
-        sys.exit(1)
-    temp_logger.info("Dati caricati correttamente.")
+    # --- Decidi nome run ---
+    run_name = decide_run_name(config, temp_logger)
 
-    # --- Analisi dataset ---
-    analyzer = DatasetAnalyzer(config.input.dataset_folder, temp_logger)
-    analyzer.analyze_and_report(config.dataset_parameters.min_samples_per_class)
-    temp_logger.info("Analisi dataset completata.")
+    log_level = logging.DEBUG if config.parameters.debug else logging.INFO
+
+    logger = flush_temp_logger(run_name, log_level, temp_logger)
+
+
+    # --- Inizializzazione SummaryWriter ---
+    log_dir = os.path.join("runs", run_name)
+    writer = SummaryWriter(log_dir=log_dir)
+    logger.debug(f"SummaryWriter inizializzato in: {log_dir}")
+
+    # --- Inizializzazione LoggerUtils
+    logger_utils = LoggerUtils(logger, writer)
+
+    # --- Caricamento dati ---
+    data_manager = DataLoaderManager(config, logger)
+
 
     # --- Addestramento o Test ---
-    trainer = Trainer(config, data_manager, num_classes, temp_logger)
-    logger = get_logger(run_name=get_run_name())
 
     logger.info(f"config.parameters.train = {config.parameters.train}")
     logger.info(f"config.parameters.final_test = {config.parameters.final_test}")
 
     if config.parameters.train and config.parameters.final_test:
         logger.info("Decisione: train + test finale")
+
+        data_manager.load_train_val()
+        model, criterion, device = create_model(config, len(data_manager.classes), logger)
+        trainer = Trainer(config, data_manager, run_name, logger, writer, logger_utils, model, criterion, device)
+        
+        # --- Analisi dataset ---
+        analyzer = DatasetAnalyzer(config.input.dataset_folder, logger)
+        analyzer.analyze_and_report(config.dataset_parameters.min_samples_per_class)
+        logger.info("Analisi dataset completata.")
+
+        
         logger.info(f"Numero di epoche: {config.hyper_parameters.epochs}")
         trainer.train()
-        trainer.test_model(use_current_model=True)
+
+        data_manager.load_test_only()
+        tester = Tester(config, data_manager, logger, writer, logger_utils, model, criterion, device)
+
+        tester.test_model(reload_checkpoint=False, epoch=trainer.current_epoch)
+
     elif config.parameters.train:
         logger.info("Decisione: solo train")
-        trainer.train()
-    elif config.parameters.final_test:
+
+        data_manager.load_train_val()
+        model, criterion, device = create_model(config, len(data_manager.classes), logger)
+        trainer = Trainer(config, data_manager, run_name, logger, writer, logger_utils, model, criterion, device)
+        # --- Analisi dataset ---
+        analyzer = DatasetAnalyzer(config.input.dataset_folder, logger)
+        analyzer.analyze_and_report(config.dataset_parameters.min_samples_per_class)
+        logger.info("Analisi dataset completata.")
+
+        
+        logger.info(f"Numero di epoche: {config.hyper_parameters.epochs}")
+        trainer.train()    
+
+    elif config.parameters.final_test and config.parameters.load_model:
         logger.info("Decisione: solo test finale")
-        trainer.test_model()    
+
+        data_manager.load_test_only()
+        model, criterion, device = create_model(config, len(data_manager.classes), logger)
+        tester = Tester(config, data_manager, logger, writer, logger_utils, model, criterion, device)
+        tester.test_model(reload_checkpoint=True)
+  
     else:
         logger.warning("Nessuna azione selezionata in config.parameters")
 
