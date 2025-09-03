190c0888089af81c63b1c2ddbdc19ddbcad82668
Alex <alex.cambrini@studio.unibo.it>
2025-09-03 22:11:56 +0200

    refactor: deepen custom CNN with additional conv blocks

--- networks/custom_net.py
+++ networks/custom_net.py
@@ -5,7 +5,6 @@ import torch.nn.functional as F
 class CustomCNN(nn.Module):
     def __init__(self, num_classes: int = 6, dropout: float = 0.5):
         super(CustomCNN, self).__init__()
-
         # -------------------------
         # Feature extraction layers
         # -------------------------
@@ -25,34 +24,35 @@ class CustomCNN(nn.Module):
         self.bn4 = nn.BatchNorm2d(256)
         self.pool4 = nn.MaxPool2d(2, 2)
 
-        self.adaptive_pool = nn.AdaptiveAvgPool2d((4, 4))
+        # Nuovi blocchi
+        self.conv5 = nn.Conv2d(256, 512, 3, 1, 1)
+        self.bn5 = nn.BatchNorm2d(512)
+        self.pool5 = nn.MaxPool2d(2, 2)
 
-        # -------------------------
-        # Fully connected layers modificati
-        # -------------------------
-        self.fc1 = nn.Linear(256 * 4 * 4, 256)
-        self.bn_fc1 = nn.BatchNorm1d(256)
+        self.conv6 = nn.Conv2d(512, 512, 3, 1, 1)
+        self.bn6 = nn.BatchNorm2d(512)
+        self.pool6 = nn.MaxPool2d(2, 2)
+
+        self.adaptive_pool = nn.AdaptiveAvgPool2d((2, 2))
+
+        self.fc1 = nn.Linear(512 * 2 * 2, 512)
+        self.bn_fc1 = nn.BatchNorm1d(512)
         self.dropout1 = nn.Dropout(dropout)
-        self.fc2 = nn.Linear(256, 128)
-        self.bn_fc2 = nn.BatchNorm1d(128)
+        self.fc2 = nn.Linear(512, 256)
+        self.bn_fc2 = nn.BatchNorm1d(256)
         self.dropout2 = nn.Dropout(dropout / 2)
-        self.fc3 = nn.Linear(128, num_classes)
+        self.fc3 = nn.Linear(256, num_classes)
 
     def forward(self, x):
         # -------------------------
         # Feature extraction con BatchNorm
         # -------------------------
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = self.pool1(x)
-
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = self.pool2(x)
-
-        x = F.relu(self.bn3(self.conv3(x)))
-        x = self.pool3(x)
-
-        x = F.relu(self.bn4(self.conv4(x)))
-        x = self.pool4(x)
+        x = F.relu(self.bn1(self.conv1(x))); x = self.pool1(x)
+        x = F.relu(self.bn2(self.conv2(x))); x = self.pool2(x)
+        x = F.relu(self.bn3(self.conv3(x))); x = self.pool3(x)
+        x = F.relu(self.bn4(self.conv4(x))); x = self.pool4(x)
+        x = F.relu(self.bn5(self.conv5(x))); x = self.pool5(x)
+        x = F.relu(self.bn6(self.conv6(x))); x = self.pool6(x)
 
         # -------------------------
         # Adaptive pooling e flatten
